17a7fa62c47c1da0db95c80445913a600bae19e2
richita <richarsoriamora@gmail.com>
2022-10-30 21:57:44 -0300

    agregar func input

--- inferencia.py
+++ inferencia.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
+import pandas as pd
 
+def input_fn(data, content_type):
+    if content_type == 'json':
+        data = pd.read_json(data)
+    return data
+    
 def predict(input):
     model = tf.load('...')
     y = model.fit(input)
