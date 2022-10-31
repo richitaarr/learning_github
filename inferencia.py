5108c1c425b6eb60033475511497f9b2cb9365e9
richita <richarsoriamora@gmail.com>
2022-10-30 22:03:50 -0300

    agregar model load

--- inferencia.py
+++ inferencia.py
@@ -5,8 +5,11 @@ def input_fn(data, content_type):
     if content_type == 'json':
         data = pd.read_json(data)
     return data
-    
-def predict(input):
-    model = tf.load('...')
+
+def model_fn(dir):
+    model = tf.load_model(dir)
+    return model
+
+def predict(model, input):
     y = model.fit(input)
     return y
