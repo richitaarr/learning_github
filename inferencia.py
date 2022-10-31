06cc1da4559525f7bb56098f425afba84f7653d3
richita <richarsoriamora@gmail.com>
2022-10-30 22:26:11 -0300

    agregar exception

--- inferencia.py
+++ inferencia.py
@@ -4,7 +4,10 @@ import pandas as pd
 def input_fn(data, content_type):
     if content_type == 'json':
         data = pd.read_json(data)
-    return data
+        return data
+    Exception:
+        return 'error input json'
+    
     
 def predict(input):
     model = tf.load('...')
